abc47a1050e02db186c0652334d8791ba96b323e
duman <duman200354@mail.ru>
2022-06-24 21:45:21 +0600

    incorrect space underline

--- script.js
+++ script.js
@@ -14,15 +14,22 @@ quoteInput.addEventListener('input', () => {
         const character = arrayValue[index];
 
         if ( character == null ) {
-            characterSpan.classList.remove('correct');
-            characterSpan.classList.remove('incorrect');
+            removeStyle(characterSpan);
             correct = false;
         } else if ( character === characterSpan.innerText ) {
-            characterSpan.classList.add('correct');
-            characterSpan.classList.remove('incorrect');
+            if ( isSpace(characterSpan.innerText) ) {
+                characterSpan.classList.remove('incorrect-bg');
+            } else {
+                characterSpan.classList.add('correct');
+                characterSpan.classList.remove('incorrect');
+            }
         } else {
-            characterSpan.classList.remove('correct');
-            characterSpan.classList.add('incorrect');
+            if ( isSpace(characterSpan.innerText) ) {
+                characterSpan.classList.add('incorrect-bg');
+            } else {
+                characterSpan.classList.remove('correct');
+                characterSpan.classList.add('incorrect');
+            }
             correct = false;
         }
     });
@@ -31,6 +38,17 @@ quoteInput.addEventListener('input', () => {
         renderNewQuote();
     }
 });
+
+function removeStyle(characterSpan) {
+    characterSpan.classList.remove('correct-bg');
+    characterSpan.classList.remove('incorrect-bg');
+    characterSpan.classList.remove('correct');
+    characterSpan.classList.remove('incorrect');
+}
+
+function isSpace(character) {
+    return character == ' ';
+}
 
 function getRandomQuote() {
     return fetch(url).then(response => response.json());
